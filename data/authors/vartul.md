1c0e41bf771420860b382724c88f5a34e823d721
Lakshya Sharma <lakshyatu@gmail.com>
2022-06-20 19:00:55 +0530

    Blog

---
+++ data/authors/vartul.md
@@ -0,0 +1,10 @@
+---
+name: Vartul Sharma
+avatar: /static/images/varavatar.jpg
+occupation: Software Developer
+company: FindYourTrip
+twitter: https://twitter.com/vartul14
+linkedin: https://www.linkedin.com/in/vartul/
+---
+
+SDE at FindYourTrip
--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -5,8 +5,8 @@ const siteMetadata = {
   description: 'A blog by Lakshya and Vartul',
   language: 'en-us',
   theme: 'system', // system, dark or light
-  siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
-  siteRepo: 'https://github.com/timlrx/tailwind-nextjs-starter-blog',
+  siteUrl: 'https://www.bro-code.in/',
+  siteRepo: 'https://github.com/lakshyasharma14/bro-code',
   siteLogo: '/static/images/logo.png',
   image: '/static/images/avatar.jpg',
   socialBanner: '/static/images/twitter-card.png',
