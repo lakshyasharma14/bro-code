214069554de73ce1bbb8dcdba317f62950118179
Lakshya Sharma <lakshyatu@gmail.com>
2022-06-20 18:16:37 +0530

    BLog

---
+++ data/authors/default.md
@@ -0,0 +1,12 @@
+---
+name: Lakshya Sharma
+avatar: /static/images/avatar.jpg
+occupation: Software Developer
+company: FindYourTrip
+email: lakshyasharma14@outlook.com
+twitter: https://twitter.com/lakshya00796
+linkedin: https://www.linkedin.com/in/lakshya-sharma/
+github: https://github.com/lakshyasharma14
+---
+
+I'm a graduate in Computer Science 🎓 from Thapar University 🏛. I'm a passionate learner who's always willing to learn and work across technologies and domains 💡. I love to explore new technologies and leverage them to solve real-life problems 👨🏻‍💻. I have 3 year of backend development and I'm currently exploring Web Development 🕸️.
---
+++ data/projectsData.js
@@ -0,0 +1,20 @@
+const projectsData = [
+  {
+    title: 'Find Your Trip',
+    description: `What if you could look up any information in the world? Webpages, images, videos
+    and more. Google has many features to help you find exactly what you're looking
+    for.`,
+    imgSrc: '/static/images/fyt logo.png',
+    href: 'https://www.findyourtrip.in/',
+  },
+  {
+    title: 'Youtube Series',
+    description: `Imagine being able to travel back in time or to the future. Simple turn the knob
+    to the desired date and press "Go". No more worrying about lost keys or
+    forgotten headphones with this simple yet affordable solution.`,
+    imgSrc: '/static/images/youtubech.png',
+    href: 'https://www.youtube.com/channel/UCAbqAkaSXFIsWShtl51THQQ',
+  },
+]
+
+export default projectsData
